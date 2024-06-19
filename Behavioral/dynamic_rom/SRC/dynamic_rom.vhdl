66f85ea2acedad7b6b80420dc8ed33448acb8d95
Sajad Dehqan <prisoner.sedwna@gmail.com>
2024-06-19 10:14:20 +0330

    fix bug

--- Behavioral/dynamic_rom/SRC/dynamic_rom.vhdl
+++ Behavioral/dynamic_rom/SRC/dynamic_rom.vhdl
@@ -7,7 +7,6 @@ ENTITY dynamic_rom IS
         adr_size : INTEGER := 3;
         data_size : INTEGER := 16);
     PORT (
-        clk, re : IN STD_LOGIC;--clock and read enable
         adr : IN STD_LOGIC_VECTOR(adr_size - 1 DOWNTO 0);
         data : OUT STD_LOGIC_VECTOR(data_size - 1 DOWNTO 0)
 
@@ -24,13 +23,5 @@ ARCHITECTURE behavioral OF dynamic_rom IS
     );
 
 BEGIN
-    PROCESS (clk)
-    BEGIN
-        IF (clk = '1' AND clk'event) THEN
-            IF (re = '1') THEN
-                data <= my_dynamic_rom(CONV_INTEGER(adr));
-            END IF;
-        END IF;
-    END PROCESS;
-
+    data <= my_dynamic_rom(CONV_INTEGER(adr));
 END ARCHITECTURE;
--- Behavioral/dynamic_rom/SRC/tb_dynamic_rom.vhdl
+++ Behavioral/dynamic_rom/SRC/tb_dynamic_rom.vhdl
@@ -14,22 +14,17 @@ ARCHITECTURE test OF test_dynamic_rom IS
             data_size : INTEGER := 16);
 
         PORT (
-            clk, re : IN STD_LOGIC;--clock and read enable
             adr : IN STD_LOGIC_VECTOR(adr_size - 1 DOWNTO 0);
             data : OUT STD_LOGIC_VECTOR(data_size - 1 DOWNTO 0)
 
         );
     END COMPONENT;
     FOR ALL : dynamic_rom USE ENTITY work.dynamic_rom(behavioral);
-    SIGNAL clk : STD_LOGIC := '0';
-    SIGNAL re : STD_LOGIC;
     SIGNAL adr : STD_LOGIC_VECTOR(adr_size - 1 DOWNTO 0);
     SIGNAL data : STD_LOGIC_VECTOR(data_size - 1 DOWNTO 0);
 BEGIN
 
-    uut : dynamic_rom GENERIC MAP(adr_size, data_size) PORT MAP(clk, re, adr, data);
-    clk <= NOT clk AFTER 5 ns;
-    re <= '0', '1' AFTER 30 ns, '0' AFTER 90 ns;
+    uut : dynamic_rom GENERIC MAP(adr_size, data_size) PORT MAP(adr, data);
     adr <= "000", "001" AFTER 40 ns, "010" AFTER 50 ns, "011" AFTER 60 ns, "100" AFTER 70 ns, "101" AFTER 80 ns, "111" AFTER 90 ns;
 
 END ARCHITECTURE;
