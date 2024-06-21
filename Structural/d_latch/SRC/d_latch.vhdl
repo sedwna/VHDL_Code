8114df873ae4225051b51045374f06f08af72970
Sajad Dehqan <prisoner.sedwna@gmail.com>
2024-06-21 19:58:55 +0330

    add a d_latch code

---
+++ Structural/d_latch/SRC/d_latch.vhdl
@@ -0,0 +1,16 @@
+ENTITY d_latch IS
+    PORT (
+        clk, rst, d : IN STD_LOGIC;
+        q, q_bar : OUT BIT
+    );
+END d_latch;
+ARCHITECTURE struct OF d_latch IS
+    SIGNAL t0, t1, t2, t3 : BIT;
+BEGIN
+    t3 <= d AND c AND (NOT rst);
+    t2 <= ((NOT d) AND (c)) OR rst;
+    t1 <= t3 NOR t0;
+    t0 <= t1 NOR t2;
+    q <= t0;
+    q_bar <= t1;
+END ARCHITECTURE;
